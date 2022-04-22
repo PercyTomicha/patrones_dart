874cbcf4873fb8b2bd95720420458a576ab045f5
Percy Tomicha <mspercywillans@gmail.com>
2022-04-21 23:57:24 -0400

    main() - Cambiando el Comportamiento

--- strategy/main.dart
+++ strategy/main.dart
@@ -1,7 +1,11 @@
+import 'concrete_strategyB.dart';
 import 'context.dart';
 
 main() {
   //Comportamiento por defecto 
   Context context = Context(); 
+  context.execute();
+  //Cambiar el comportamiento 
+  context.setStrategy( ConcreteStrategyB() ); 
   context.execute();
 }
