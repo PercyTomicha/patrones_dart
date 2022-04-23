7fc77bdb28918a5f411d15ca885c9902e08e0a83
Percy Tomicha <mspercywillans@gmail.com>
2022-04-22 22:56:24 -0400

    class Componente Added

---
+++ composity/component.dart
@@ -0,0 +1,11 @@
+class Componente
+{
+	String? _nombre;
+	Componente (String nombre)
+	{
+		_nombre = nombre;
+	}
+	void agregar(Componente c) { }
+	void eliminar(Componente c) { }
+	void mostrar(int profundidad) { }
+}
