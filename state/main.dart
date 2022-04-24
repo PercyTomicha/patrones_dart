fed6331f121d47ae381da352493ed74c3e92b0bb
Percy Tomicha <mspercywillans@gmail.com>
2022-04-23 23:47:18 -0400

    Update main()

--- state/main.dart
+++ state/main.dart
@@ -1,5 +1,21 @@
-import 'dart:html';
+import 'concrete_stateA.dart';
+import 'concrete_stateB.dart';
+import 'context.dart';
+import 'state.dart';
 
 main() {
-  print('State');
+  try
+  {
+    State state = ConcreteStateA();
+    Context context = Context();
+    context.setState( state ); 
+    context.request();
+    State state1 = ConcreteStateB();
+    context.setState( state1 ); 
+    context.request();
+  }
+  catch( e ) 
+  {
+    print('Error ${e}');
+  }
 }
