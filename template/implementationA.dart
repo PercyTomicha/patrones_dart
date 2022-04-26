1a986a8a1207c4075328f2658da072ffe37df35b
Percy Tomicha <mspercywillans@gmail.com>
2022-04-25 23:17:23 -0400

    ImplementationA Updated

---
+++ template/implementationA.dart
@@ -0,0 +1,16 @@
+import 'abstract_template.dart';
+
+class ImplementationA extends AbstractTemplate {
+  @override
+  void step1(){
+    print('Implementation A - Step #1');
+  }
+  @override
+  void step2(){
+    print('Implementation A - Step #2');
+  }
+  @override
+  void step3(){
+    print('Implementation A - Step #3');
+  }
+}
