2aa907faeb8c46113a0431a999c6cbd6f4179cda
Percy Tomicha <mspercywillans@gmail.com>
2022-04-25 23:18:34 -0400

    Template main() Updated

--- template/main.dart
+++ template/main.dart
@@ -1,3 +1,10 @@
+import 'abstract_template.dart';
+import 'implementationA.dart';
+import 'implementationB.dart';
+
 main() {
   print('Template');
+  // AbstractTemplate abstractTemplate = ImplementationA();
+  AbstractTemplate abstractTemplate = ImplementationB();
+  abstractTemplate.templateMethod();
 }
