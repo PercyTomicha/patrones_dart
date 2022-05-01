9f1a0b3568297eb55f6fdf74618e4de3c744b9b1
Percy Tomicha <mspercywillans@gmail.com>
2022-04-30 23:34:41 -0400

    Prototype main() Updated v2

--- prototype/main.dart
+++ prototype/main.dart
@@ -8,4 +8,19 @@ void main() {
   print("ogConcretePrototype1 ${ogRect.cloneStatus}.");
   print("cloneConcretePrototype1 ${cloneRect.cloneStatus}.");
   print("someOtherConcretePrototype1 ${someOtherRect.cloneStatus}.");
+
+  String cloneIsClone = ogRect == cloneRect ? "is a clone of" : "is not a clone of";
+  print("\r\ncloneConcretePrototype1 $cloneIsClone ogConcretePrototype1.");
+
+  String someRectIsClone = ogRect == someOtherRect ? "is a clone of" : "is not a clone of";
+  print("someOtherConcretePrototype1 $someRectIsClone ogConcretePrototype1.");
+
+  /*
+    ogConcretePrototype1 is an original gangster.
+    cloneConcretePrototype1 is a clone.
+    someOtherConcretePrototype1 is an original gangster.
+
+    cloneConcretePrototype1 is a clone of ogConcretePrototype1.
+    someOtherConcretePrototype1 is not a clone of ogConcretePrototype1.
+  */
 }
