9f536db162dcf86cb44b12b61a137ea89e608046
Percy Tomicha <mspercywillans@gmail.com>
2022-04-30 23:34:11 -0400

    Prototype main() Updated

--- prototype/main.dart
+++ prototype/main.dart
@@ -4,4 +4,8 @@ void main() {
   ConcretePrototype1 ogRect = ConcretePrototype1(0, 0);
   ConcretePrototype1 cloneRect = ogRect.clone();
   var someOtherRect = ConcretePrototype1(0, 0);
+
+  print("ogConcretePrototype1 ${ogRect.cloneStatus}.");
+  print("cloneConcretePrototype1 ${cloneRect.cloneStatus}.");
+  print("someOtherConcretePrototype1 ${someOtherRect.cloneStatus}.");
 }
